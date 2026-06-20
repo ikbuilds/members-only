a2eb4a3dd87293cb89592ceb71fe3b050c16bda5
TheDeveloper <171641141+JusticeCodes00@users.noreply.github.com>
2026-06-20 03:32:10 +0100

    refactor: update auth routes and add auth views

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,11 +1,19 @@
 import { Router } from "express";
-import { login, signup, logout } from "../controller/authController.js";
+import {
+  login,
+  signup,
+  logout,
+  getSignup,
+  getLogin,
+} from "../controller/authController.js";
 import { loginValidator, signupValidator } from "../middleware/validators.js";
 
 const authRouter = Router();
 
+authRouter.get("/signup", getSignup);
 authRouter.post("/signup", signupValidator, signup);
-authRouter.post("/login", loginValidator, login);
+authRouter.get("/login", getLogin);
+authRouter.post("/login", login);
 
 authRouter.get("/logout", logout);
 
